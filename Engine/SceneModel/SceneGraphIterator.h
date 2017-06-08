1644e19d63f3dabe54a419e71735f51cc66a8317
shengchongshan <shengchongshan@sensetime.com>
2017-06-09 01:57:19 +0800

    add header guard

---
+++ Engine/SceneModel/SceneGraphIterator.h
@@ -0,0 +1,11 @@
+/*
+* Copyright 2017 sheng chongshan. All rights reserved.
+* email: shany.sheng@qq.com
+* License: https://github.com/shanysheng/RenderPipeline/master/LICENSE
+*/
+
+#ifndef PIPELINE_SCENEGRAPHITERATOR_H_GUARD
+#define PIPELINE_SCENEGRAPHITERATOR_H_GUARD
+
+
+#endif
---
+++ Engine/SceneModel/SpatialKD2TreeIterator.h
@@ -0,0 +1,11 @@
+/*
+* Copyright 2017 sheng chongshan. All rights reserved.
+* email: shany.sheng@qq.com
+* License: https://github.com/shanysheng/RenderPipeline/master/LICENSE
+*/
+
+#ifndef PIPELINE_SPATIALKD2TREEITERATOR_H_GUARD
+#define PIPELINE_SPATIALKD2TREEITERATOR_H_GUARD
+
+
+#endif
