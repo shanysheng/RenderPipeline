a44a6d5935892fd7f066a32c50de069f871e09e1
shengchongshan <shengchongshan@sensetime.com>
2017-06-09 02:08:42 +0800

    add header guard

--- Engine/RenderingEngine.h
+++ Engine/RenderingEngine.h
@@ -1,7 +1,7 @@
 /*
 * Copyright 2017 sheng chongshan. All rights reserved.
 * email: shany.sheng@qq.com
-* License: https://github.com/shanysheng/RenderPipeline/bob/master/LICENSE
+* License: https://github.com/shanysheng/RenderPipeline/blob/master/LICENSE
 */
 
 #ifndef PIPELINE_RENDERINGENGINE_H_GUARD
