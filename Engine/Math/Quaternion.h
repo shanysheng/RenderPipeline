6eca5ad6c6bf37770d28339bf8f73e5d6fb4ef1d
shengchongshan <shengchongshan@sensetime.com>
2017-06-09 02:18:24 +0800

    add math file

---
+++ Engine/Math/Quaternion.h
@@ -0,0 +1,11 @@
+/*
+* Copyright 2017 sheng chongshan. All rights reserved.
+* email: shany.sheng@qq.com
+* License: https://github.com/shanysheng/RenderPipeline/bob/master/LICENSE
+*/
+
+#ifndef PIPELINE_QUATERNION_H_GUARD
+#define PIPELINE_QUATERNION_H_GUARD
+
+
+#endif
