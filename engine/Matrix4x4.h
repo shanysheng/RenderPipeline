a0899a7438c912443eabc907c70ac6eb51e7843b
shengchongshan <shengchongshan@sensetime.com>
2017-06-17 00:50:08 +0800

    Update quaternion and matrix definition

--- engine/Matrix4x4.h
+++ engine/Matrix4x4.h
@@ -23,29 +23,49 @@ namespace pipeline{
         static const Matrix4x4f identity;
 
     public:
-        void SetAxisX( const Vector3f& v );
-        void SetAxisY( const Vector3f& v );
-        void SetAxisZ( const Vector3f& v );
-        void SetAxis( int axis, const Vector3f& v );
-        void SetPosition( const Vector3f& v );
-        void SetRow( int row, const Vector4f& v );
-        void SetColumn( int col, const Vector4f& v );
+        Matrix4x4f();
+        Matrix4x4f(const Matrix4x4f& rhs);
+        
+        Matrix4x4f operator* (const Matrix4x4f& rhs) const;
+        void operator*=(const Matrix4x4f& rhs);
+        Matrix4x4f operator+ (const Matrix4x4f& rhs) const;
+        void operator+=(const Matrix4x4f& rhs);
+        Matrix4x4f operator* (float scale) const;
+        void operator*=(float scale);
+        inline bool operator==(const Matrix4x4f& rhs) const;
+        inline bool Equals(const Matrix4x4f& rhs, float tolerance) const;
+        inline bool operator!=(const Matrix4x4f& rhs) const;
+        Vector4f TransformVector4f(const Vector4f& vec4) const;
+        Vector4f TransformPosition(const Vector3f& vec3) const;
+        Vector3f InverseTransformPosition(const Vector3f& vec3) const;
+        Vector4f TransformVector(const Vector3f& vec3) const;
+        Vector3f InverseTransformVector(const Vector3f& vec3) const;
+        Matrix4x4f GetTransposed() const;
+        inline float Determinant() const;
+        Matrix4x4f Inverse() const;
+        
+        void SetAxisX( const Vector3f& vec3 );
+        void SetAxisY( const Vector3f& vec3 );
+        void SetAxisZ( const Vector3f& vec3 );
+        void SetAxis( int axis, const Vector3f& vec3 );
+        void SetPosition( const Vector3f& vec3 );
+        void SetRow( int row, const Vector4f& vec3 );
+        void SetColumn( int col, const Vector4f& vec3 );
         
         void SetTR (const Vector3f& pos, const Quaternionf& q);
         void SetTRS (const Vector3f& pos, const Quaternionf& q, const Vector3f& s);
         
         Vector3f MultiplyVector3 (const Vector3f& inV) const;
-        void MultiplyVector3 (const Vector3f& inV, Vector3f& output) const;
         Vector4f MultiplyVector4 (const Vector4f& inV) const;
-        void MultiplyVector4 (const Vector4f& inV, Vector4f& output) const;
         Vector3f MultiplyPoint3 (const Vector3f& inV) const;
+
+        void MultiplyVector3 (const Vector3f& inV, Vector3f& output) const;
+        void MultiplyVector4 (const Vector4f& inV, Vector4f& output) const;
         void MultiplyPoint3 (const Vector3f& inV, Vector3f& output) const;
         
         Matrix4x4f& SetIdentity ();
         Matrix4x4f& SetPerspective( float fovy, float aspect, float zNear, float zFar );
         Matrix4x4f& SetPerspective( float left, float right, float top, float bottom, float zNear, float zFar );
-        // rad = Deg2Rad(fovy/2), contanHalfFOV = cos(rad)/sin(rad)
-        Matrix4x4f& SetPerspectiveCotan( float cotanHalfFOV, float zNear, float zFar );
         Matrix4x4f& SetOrtho( float left, float right, float bottom, float top, float zNear, float zFar );
         Matrix4x4f& SetFrustum( float left, float right, float bottom, float top, float nearval, float farval );
     };
--- engine/Quaternion.h
+++ engine/Quaternion.h
@@ -7,12 +7,54 @@
 #ifndef PIPELINE_QUATERNION_H_GUARD
 #define PIPELINE_QUATERNION_H_GUARD
 
+#include "Common.h"
+#include "Vector3.h"
+
 namespace pipeline{
+    
+    class Matrix4x4f;
 
     class Quaternionf
     {
     public:
         float x,y,z,w;
+        
+    public:
+        Quaternionf () ;
+        Quaternionf (float inX, float inY, float inZ, float inW);
+        Quaternionf& operator=(const Quaternionf& Other);
+        void Set (float inX, float inY, float inZ, float inW);
+        void Set (const Quaternionf& aQuat);
+  
+        Quaternionf operator+(const Quaternionf& rhs) const;
+        Quaternionf operator+=(const Quaternionf& rhs);
+        Quaternionf operator-(const Quaternionf& rhs) const;
+        bool Equals(const Quaternionf& rhs) const;
+        bool IsIdentity(float tolerance) const;
+        Quaternionf operator-=(const Quaternionf& rhs);
+        Quaternionf operator*(const Quaternionf& rhs) const;
+        Quaternionf operator*=(const Quaternionf& rhs);
+        Vector3f operator*(const Vector3f& vec) const;
+        Matrix4x4f operator*(const Matrix4x4f& mat) const;
+        Quaternionf operator*=(const float scale);
+        Quaternionf operator*(const float scale) const;
+        Quaternionf operator/=(const float scale);
+        Quaternionf operator/(const float scale) const;
+        bool operator==(const Quaternionf& rhs) const;
+        bool operator!=(const Quaternionf& rhs) const;
+        
+        void Normalize();
+        void Inverse ();
+        
+        static Quaternionf identity ();
+        static Quaternionf Normalize(const Quaternionf& q);
+        static Quaternionf Inverse (const Quaternionf& q);
+        static Vector3f QuaternionToEuler (const Quaternionf& quat);
+        static Quaternionf EulerToQuaternion (const Vector3f& euler);
+        static void QuaternionToMatrix (const Quaternionf& q, Matrix4x4f& m);
+        static void MatrixToQuaternion (const Matrix4x4f& m, Quaternionf& q);
     };
+    
+
 }
 #endif
