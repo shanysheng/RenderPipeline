608d4758b314110e6fd04e498aa8b476e7b42221
shengchongshan <shengchongshan@sensetime.com>
2017-06-24 01:47:00 +0800

    Add render control unit base class

---
+++ engine/IRenderControlUnit.cpp
@@ -0,0 +1,130 @@
+#include "IRenderControlUnit.h"
+
+namespace pipeline{
+    
+    CFrameInfo::CFrameInfo()
+    {
+        FrameIndex = 0;
+        SynTimeMS = 0;
+    }
+    
+    IRenderControlUnit::IRenderControlUnit()
+    {
+        m_bStopped = false;
+        m_pNextRCU = NULL;
+    }
+    
+    IRenderControlUnit::~IRenderControlUnit()
+    {
+        m_bStopped = false;
+        m_pNextRCU = NULL;
+        m_ChildrenRCU.clear();
+    }
+    
+    void IRenderControlUnit::_DoPerFrame( CFrameInfo * pFI )
+    {
+        if(IsStopped())
+            return ;
+        
+        PerFrame(pFI);
+        
+        for(uint32_t i = 0;i<m_ChildrenRCU.size();++i)
+            m_ChildrenRCU[i]->_DoPerFrame(pFI);
+        
+        if(!m_ChildrenRCU.empty())
+            PostChildrenPerFrame(pFI);
+        
+        if(m_pNextRCU)
+            m_pNextRCU->_DoPerFrame(pFI);
+    }
+    
+    void IRenderControlUnit::_DoBeforeFirstFrame( CFrameInfo * pFI )
+    {
+        if(IsStopped())
+            return;
+        
+        BeforeFirstFrame(pFI);
+        
+        for(uint32_t i = 0;i<m_ChildrenRCU.size();++i)
+            m_ChildrenRCU[i]->_DoBeforeFirstFrame(pFI);
+        
+        if(!m_ChildrenRCU.empty())
+            PostChildrenBeforeFirstFrame(pFI);
+        
+        if(m_pNextRCU)
+            m_pNextRCU->_DoBeforeFirstFrame(pFI);
+    }
+    
+    void IRenderControlUnit::_DoAfterLastFrame( CFrameInfo * pFI )
+    {
+        if(IsStopped())
+            return;
+        
+        AfterLastFrame(pFI);
+        
+        for(uint32_t i = 0;i<m_ChildrenRCU.size();++i)
+            m_ChildrenRCU[i]->_DoAfterLastFrame(pFI);
+        
+        if(!m_ChildrenRCU.empty())
+            PostChildrenAfterLastFrame(pFI);
+        
+        if(m_pNextRCU)
+            m_pNextRCU->_DoAfterLastFrame(pFI);
+    }
+    
+    void IRenderControlUnit::Stop ()
+    {
+        m_bStopped = true;
+        
+        for(uint32_t i = 0;i<m_ChildrenRCU.size();++i)
+            m_ChildrenRCU[i]->Stop();
+        
+        if(m_pNextRCU)
+            m_pNextRCU->Stop();
+    }
+    
+    void IRenderControlUnit::Start()
+    {
+        m_bStopped = false;
+        
+        for(uint32_t i = 0;i<m_ChildrenRCU.size();++i)
+            m_ChildrenRCU[i]->Start();
+        
+        if(m_pNextRCU)
+            m_pNextRCU->Start();
+    }
+    
+    void IRenderControlUnit::SetNextRCU( IRenderControlUnit* pNextRCU )
+    {
+        m_pNextRCU = pNextRCU;
+    }
+    
+    void IRenderControlUnit::AppendChildrenRCU(IRenderControlUnit * pRCUnit )
+    {
+        m_ChildrenRCU.push_back(pRCUnit);
+    }
+    
+    void IRenderControlUnit::PerFrame( CFrameInfo * pFI )
+    {
+    }
+    
+    void IRenderControlUnit::BeforeFirstFrame( CFrameInfo * pFI )
+    {
+    }
+    
+    void IRenderControlUnit::AfterLastFrame( CFrameInfo * pFI )
+    {
+    }
+    
+    void IRenderControlUnit::PostChildrenPerFrame(CFrameInfo * pFI )
+    {
+    }
+    
+    void IRenderControlUnit::PostChildrenBeforeFirstFrame( CFrameInfo * pFI )
+    {
+    }
+    
+    void IRenderControlUnit::PostChildrenAfterLastFrame( CFrameInfo * pFI )
+    {
+    }
+}
--- engine/IRenderControlUnit.h
+++ engine/IRenderControlUnit.h
@@ -1,11 +1,59 @@
 /*
-* Copyright 2017 sheng chongshan. All rights reserved.
-* email: shany.sheng@qq.com
-* License: https://github.com/shanysheng/RenderPipeline/blob/master/LICENSE
-*/
+ * Copyright 2017 sheng chongshan. All rights reserved.
+ * email: shany.sheng@qq.com
+ * License: https://github.com/shanysheng/RenderPipeline/blob/master/LICENSE
+ */
+
 
 #ifndef PIPELINE_IRENDERCONTROLUNIT_H_GUARD
 #define PIPELINE_IRENDERCONTROLUNIT_H_GUARD
 
+#include "Common.h"
+
+namespace pipeline{
+    
+    struct CFrameInfo
+    {
+        CFrameInfo();
+        
+        uint64_t FrameIndex;
+        double	 SynTimeMS;
+    };
+    
+    class IRenderControlUnit
+    {
+    public:
+        IRenderControlUnit();
+        virtual ~IRenderControlUnit();
+        
+        virtual void PerFrame( CFrameInfo * pFI );
+        virtual void BeforeFirstFrame( CFrameInfo * pFI );
+        virtual void AfterLastFrame( CFrameInfo * pFI );
+        virtual void PostChildrenPerFrame(CFrameInfo * pFI );
+        virtual void PostChildrenBeforeFirstFrame( CFrameInfo * pFI );
+        virtual void PostChildrenAfterLastFrame( CFrameInfo * pFI );
+        
+        virtual void Stop ();
+        virtual void Start();
+        bool         IsStopped() const { return m_bStopped; }
+        
+        void	SetNextRCU( IRenderControlUnit* pNextRCU );
+        void	AppendChildrenRCU(IRenderControlUnit * pRCUnit );
+        
+    protected:
+        
+        virtual void _DoPerFrame( CFrameInfo * pFI );
+        virtual void _DoBeforeFirstFrame( CFrameInfo * pFI );
+        virtual void _DoAfterLastFrame( CFrameInfo * pFI );
+        
+    private:
+        friend class CRenderingEngine;
+
+    protected:
+        bool m_bStopped;
+        IRenderControlUnit* m_pNextRCU;
+        std::vector<IRenderControlUnit*> m_ChildrenRCU;
+    };
+}
 
 #endif
