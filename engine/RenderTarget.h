38c1353ad1ec4980926b043fa3f3a05c3c4838b7
shengchongshan <shengchongshan@sensetime.com>
2017-06-24 11:17:36 +0800

    Add render target

--- engine/RenderTarget.h
+++ engine/RenderTarget.h
@@ -7,5 +7,30 @@
 #ifndef PIPELINE_RENDERTARGET_H_GUARD
 #define PIPELINE_RENDERTARGET_H_GUARD
 
+namespace pipeline {
+    
+    class RenderTarget
+    {
+    public:
+        RenderTarget(){}
+        virtual ~RenderTarget(){}
+        
+        virtual int		GetType() = 0;
+        
+        virtual void	BindRenderTarget() = 0;
+        virtual void	UnBindRenderTarget() = 0;
+        
+        virtual int		SetOutputDepthTexture(uint32_t){ return false; }
+        virtual int		SetOutputColorTextureCount(uint32_t Count ) {return false;}
+        
+        virtual uint32_t GetRenderTargetTextureId() const {return 0;}
+        virtual uint32_t GetDepthTargetTextureId() const {return 0;}
+        
+        virtual int		CreateRenderTexture(uint32_t in_Width, uint32_t in_Height) {return false;}
+        virtual int     ResizeRenderTexture(uint32_t in_Width, uint32_t in_Height) {return false;}
+        
+        virtual int		CreateRenderWindow( const CWinInfo& WinInfo) {return false;}
+    };
+}
 
 #endif
