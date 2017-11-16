bbd8ab59f0da45626192e755fc03d6926dddd3be
shanysheng <shany.sheng@qq.com>
2017-11-17 00:36:27 +0800

    update shader

--- engine/Shader.cpp
+++ engine/Shader.cpp
@@ -3,4 +3,278 @@
 namespace pipeline {
     
     
+    Shader::Shader()
+    {
+        
+    }
+    
+    
+    void Shader::Bind() const
+    {
+        
+    }
+    
+    void Shader::Unbind() const
+    {
+        
+    }
+    
+    
+    std::string Shader::IDToProperty (int id)
+    {
+        return "";
+    }
+    
+    bool Shader::IsKeywordEnabled (const std::string& keyword)
+    {
+        return false;
+    }
+    
+    
+    Color Shader::GetGlobalColor (const std::string& name)
+    {
+        return Color();
+    }
+    
+    Color Shader::GetGlobalColor (int nameID)
+    {
+        return Color();
+    }
+    
+    float Shader::GetGlobalFloat (int nameID)
+    {
+        return 0.0f;
+    }
+    
+    float Shader::GetGlobalFloat (const std::string& name)
+    {
+        return 0.0f;
+    }
+    
+    void Shader::GetGlobalFloatArray (int nameID, std::vector<float>& values)
+    {
+        
+    }
+    
+    void Shader::GetGlobalFloatArray (const std::string& name, std::vector<float>& values)
+    {
+        
+    }
+    
+    float* Shader::GetGlobalFloatArray (int nameID)
+    {
+        return NULL;
+    }
+    
+    
+    int Shader::GetGlobalInt (const std::string& name)
+    {
+        return 0;
+    }
+    
+    int Shader::GetGlobalInt (int nameID)
+    {
+        return 0;
+    }
+    
+    int Shader::GetGlobalIntImpl (int nameID)
+    {
+        return 0;
+    }
+    
+    
+    Matrix4x4f Shader::GetGlobalMatrix (int nameID)
+    {
+        return Matrix4x4f();
+    }
+    
+    Matrix4x4f Shader::GetGlobalMatrix (const std::string& name)
+    {
+        return Matrix4x4f();
+    }
+    
+    
+    void Shader::GetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f>& values)
+    {
+        
+    }
+    
+    Matrix4x4f* Shader::GetGlobalMatrixArray (const std::string& name)
+    {
+        return NULL;
+    }
+    
+    void Shader::GetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f>& values)
+    {
+        
+    }
+    
+    Matrix4x4f* Shader::GetGlobalMatrixArray (int nameID)
+    {
+        return NULL;
+    }
+    
+    
+    Texture* Shader::GetGlobalTexture (int nameID)
+    {
+        return NULL;
+    }
+    
+    Texture* Shader::GetGlobalTexture (const std::string& name)
+    {
+        return NULL;
+    }
+    
+    Vector4f Shader::GetGlobalVector (int nameID)
+    {
+        return Vector4f();
+    }
+    
+    Vector4f Shader::GetGlobalVector (const std::string& name)
+    {
+        return Vector4f();
+    }
+    
+    void Shader::GetGlobalVectorArray (const std::string& name, std::vector<Vector4f>& values)
+    {
+        
+    }
+    
+    void Shader::GetGlobalVectorArray (int nameID, std::vector<Vector4f>& values)
+    {
+        
+    }
+    
+    Vector4f* Shader::GetGlobalVectorArray (int nameID)
+    {
+        return NULL;
+    }
+    
+    Vector4f* Shader::GetGlobalVectorArray (const std::string& name)
+    {
+        return NULL;
+    }
+    
+    
+    int Shader::PropertyToID (const std::string& name)
+    {
+        return 0;
+    }
+    
+    void Shader::SetGlobalColor (const std::string& name, const Color& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalColor (int nameID, const Color& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloat (int nameID, float value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloat (const std::string& name, float value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloatArray (int nameID, std::vector<float>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloatArray (const std::string& name, std::vector<float>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloatArray (int nameID, float* values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalFloatArray (const std::string& name, float* values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalInt (const std::string& name, int value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalInt (int nameID, int value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrix (const std::string& name, const Matrix4x4f& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrix (int nameID, const Matrix4x4f& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrixArray (const std::string& name, Matrix4x4f* values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalMatrixArray (int nameID, Matrix4x4f* values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalTexture (const std::string& name, Texture* value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVector (int nameID, const Vector4f& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVector (const std::string& name, const Vector4f& value)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVectorArray (int nameID, std::vector<Vector4f>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVectorArray (const std::string& name, std::vector<Vector4f>& values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVectorArray (int nameID, Vector4f* values)
+    {
+        
+    }
+    
+    void Shader::SetGlobalVectorArray (const std::string& name, Vector4f* values)
+    {
+        
+    }
+    
+    
 }
--- engine/Shader.h
+++ engine/Shader.h
@@ -22,8 +22,69 @@ namespace pipeline {
     class Shader
     {
     public:
+        Shader();
+        
         virtual void Bind() const;
         virtual void Unbind() const;
+        
+        static std::string IDToProperty (int id);
+        static bool IsKeywordEnabled (const std::string& keyword);
+        
+        static Color GetGlobalColor (const std::string& name);
+        static Color GetGlobalColor (int nameID);
+        static float GetGlobalFloat (int nameID);
+        static float GetGlobalFloat (const std::string& name);
+        static void GetGlobalFloatArray (int nameID, std::vector<float>& values);
+        static void GetGlobalFloatArray (const std::string& name, std::vector<float>& values);
+        static float* GetGlobalFloatArray (int nameID);
+        
+        static int GetGlobalInt (const std::string& name);
+        static int GetGlobalInt (int nameID);
+        static int GetGlobalIntImpl (int nameID);
+
+        static Matrix4x4f GetGlobalMatrix (int nameID);
+        static Matrix4x4f GetGlobalMatrix (const std::string& name);
+        
+        static void GetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f>& values);
+        static Matrix4x4f* GetGlobalMatrixArray (const std::string& name);
+        static void GetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f>& values);
+        static Matrix4x4f* GetGlobalMatrixArray (int nameID);
+        
+        static Texture* GetGlobalTexture (int nameID);
+        static Texture* GetGlobalTexture (const std::string& name);
+        static Vector4f GetGlobalVector (int nameID);
+        static Vector4f GetGlobalVector (const std::string& name);
+        static void GetGlobalVectorArray (const std::string& name, std::vector<Vector4f>& values);
+        static void GetGlobalVectorArray (int nameID, std::vector<Vector4f>& values);
+        static Vector4f* GetGlobalVectorArray (int nameID);
+        static Vector4f* GetGlobalVectorArray (const std::string& name);
+
+        static int PropertyToID (const std::string& name);
+        static void SetGlobalColor (const std::string& name, const Color& value);
+        static void SetGlobalColor (int nameID, const Color& value);
+        static void SetGlobalFloat (int nameID, float value);
+        static void SetGlobalFloat (const std::string& name, float value);
+        static void SetGlobalFloatArray (int nameID, std::vector<float>& values);
+        static void SetGlobalFloatArray (const std::string& name, std::vector<float>& values);
+        static void SetGlobalFloatArray (int nameID, float* values);
+        static void SetGlobalFloatArray (const std::string& name, float* values);
+        static void SetGlobalInt (const std::string& name, int value);
+        static void SetGlobalInt (int nameID, int value);
+        static void SetGlobalMatrix (const std::string& name, const Matrix4x4f& value);
+        static void SetGlobalMatrix (int nameID, const Matrix4x4f& value);
+        static void SetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f>& values);
+        static void SetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f>& values);
+        static void SetGlobalMatrixArray (const std::string& name, Matrix4x4f* values);
+        static void SetGlobalMatrixArray (int nameID, Matrix4x4f* values);
+        static void SetGlobalTexture (const std::string& name, Texture* value);
+        static void SetGlobalVector (int nameID, const Vector4f& value);
+        static void SetGlobalVector (const std::string& name, const Vector4f& value);
+        static void SetGlobalVectorArray (int nameID, std::vector<Vector4f>& values);
+        static void SetGlobalVectorArray (const std::string& name, std::vector<Vector4f>& values);
+        static void SetGlobalVectorArray (int nameID, Vector4f* values);
+        static void SetGlobalVectorArray (const std::string& name, Vector4f* values);
+        
+    protected:
         
         //
         // Static Properties
@@ -35,62 +96,6 @@ namespace pipeline {
         int maximumLOD;
         int renderQueue;
 
-        static const std::string& IDToProperty (int id);
-        static bool IsKeywordEnabled (const std::string& keyword);
-        
-        static Color GetGlobalColor (const std::string& name);
-        static Color GetGlobalColor (int nameID);
-        static float GetGlobalFloat (int nameID);
-        static float GetGlobalFloat (const std::string& name);
-        static void GetGlobalFloatArray (int nameID, std::vector<float> values);
-        static void GetGlobalFloatArray (const std::string& name, std::vector<float> values);
-        static float* GetGlobalFloatArray (int nameID);
-        
-        static int GetGlobalInt (const std::string& name);
-        static int GetGlobalInt (int nameID);
-        static int GetGlobalIntImpl (int nameID);
-
-        static Matrix4x4f GetGlobalMatrix (int nameID);
-        static Matrix4x4f GetGlobalMatrix (const std::string& name);
-        
-        static void GetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f> values);
-        static Matrix4x4f* GetGlobalMatrixArray (const std::string& name);
-        static void GetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f> values);
-        static Matrix4x4f* GetGlobalMatrixArray (int nameID);
-        
-        static Texture GetGlobalTexture (int nameID);
-        static Texture GetGlobalTexture (const std::string& name);
-        static Vector4f GetGlobalVector (int nameID);
-        static Vector4f GetGlobalVector (const std::string& name);
-        static void GetGlobalVectorArray (const std::string& name, std::vector<Vector4f> values);
-        static void GetGlobalVectorArray (int nameID, std::vector<Vector4f> values);
-        static Vector4f* GetGlobalVectorArray (int nameID);
-        static Vector4f* GetGlobalVectorArray (const std::string& name);
-
-        static int PropertyToID (const std::string& name);
-        static void SetGlobalColor (const std::string& name, Color value);
-        static void SetGlobalColor (int nameID, Color value);
-        static void SetGlobalFloat (int nameID, float value);
-        static void SetGlobalFloat (const std::string& name, float value);
-        static void SetGlobalFloatArray (int nameID, std::vector<float> values);
-        static void SetGlobalFloatArray (const std::string& name, std::vector<float> values);
-        static void SetGlobalFloatArray (int nameID, float* values);
-        static void SetGlobalFloatArray (const std::string& name, float* values);
-        static void SetGlobalInt (const std::string& name, int value);
-        static void SetGlobalInt (int nameID, int value);
-        static void SetGlobalMatrix (const std::string& name, Matrix4x4f value);
-        static void SetGlobalMatrix (int nameID, Matrix4x4f value);
-        static void SetGlobalMatrixArray (int nameID, std::vector<Matrix4x4f> values);
-        static void SetGlobalMatrixArray (const std::string& name, std::vector<Matrix4x4f> values);
-        static void SetGlobalMatrixArray (const std::string& name, Matrix4x4f* values);
-        static void SetGlobalMatrixArray (int nameID, Matrix4x4f* values);
-        static void SetGlobalTexture (const std::string& name, Texture value);
-        static void SetGlobalVector (int nameID, Vector4f value);
-        static void SetGlobalVector (const std::string& name, Vector4f value);
-        static void SetGlobalVectorArray (int nameID, std::vector<Vector4f> values);
-        static void SetGlobalVectorArray (const std::string& name, std::vector<Vector4f> values);
-        static void SetGlobalVectorArray (int nameID, Vector4f* values);
-        static void SetGlobalVectorArray (const std::string& name, Vector4f* values);
     };
 }
 
